0fefc1c188a7e6e687893376a09c9c669328853a
shrke <shreyamkela@gmail.com>
2019-03-06 03:05:34 -0800

    started lab1

--- lab0/src/com/cmpe275/lab0/Greeter.java
+++
@@ -1,7 +0,0 @@
-package com.cmpe275.lab0;
-
-public interface Greeter { // Interface is in the same package therefore would run even without public
-	void setName(String name); // name of the author - Shreyam Kela
-	String getGreeting();
-}
-
